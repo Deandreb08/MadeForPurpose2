6fc9abad37bac0a1a82746405bde034c4857323f
deandreb <deandreb08@gmail.com>
2016-08-08 15:49:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dsuappacademy/newstudent/Event.java
@@ -0,0 +1,21 @@
+package com.example.dsuappacademy.newstudent;
+
+/**
+ * Created by dsuappacademy on 8/8/16.
+ */
+public class Event {
+    private int time;
+    private String title;
+    private String location;
+    private String discription;
+
+    public Event(int time, String title, String location, String discription){
+        this.time = time;
+        this.title = title;
+        this.location = location;
+        this.discription = discription;
+    }
+
+    public int getTime{return time;}
+}
+
